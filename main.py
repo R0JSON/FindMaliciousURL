9357c98ea787b085deb33209b1867f6adb5dc071
msiergiej <89871398+msiergiej@users.noreply.github.com>
2026-06-05 20:41:48 +0200

    eksperyment 1

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from wordcloud import WordCloud
+from sklearn.model_selection import RepeatedStratifiedKFold, cross_validate
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import get_scorer_names
+
+#from wordcloud import WordCloud
 def load_dataset(path: str):
     df = pd.read_csv(path)
     # print(df.shape)
@@ -26,6 +32,22 @@ def visulize_dataset(df):
     plt.tight_layout()
     plt.show()
 
+def compare_classifiers(df):
+    x = df.drop(columns=["url", "dom", "label", "tld"])
+    y = df["label"]
+
+    models = [GaussianNB(), DecisionTreeClassifier(), RandomForestClassifier()]
+    rskf = RepeatedStratifiedKFold(n_repeats=2, n_splits=5)
+    results = []
+
+    for model in models:
+        scores = cross_validate(model, x, y, cv=rskf, scoring=["balanced_accuracy"])
+        results.append([model.__class__.__name__ , scores["test_balanced_accuracy"].mean()])
+
+    return results
+
 if __name__ == "__main__":
-    df = load_dataset("./Dataset.csv")
-    visulize_dataset(df)
+    df = load_dataset("C:\\Users\\oem\\Desktop\\ai\\FindMaliciousURL\\Dataset.csv")
+    #visulize_dataset(df)
+    print(df)
+    print(compare_classifiers(df))
